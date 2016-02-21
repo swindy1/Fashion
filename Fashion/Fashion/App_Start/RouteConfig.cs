39666547856fec10e02067cf399fedf126add808
111 <222>
2016-02-21 20:40:40 +0800

    2016年2月21日添加媛阳做好的Topic-Home页面,准备实现后端

--- Fashion/Fashion/App_Start/RouteConfig.cs
+++ Fashion/Fashion/App_Start/RouteConfig.cs
@@ -17,6 +17,16 @@ namespace Fashion
             //2.路由是有顺序的。前面被匹配了之后，后面的路由规则就没有机会了。
             //3.路由可以调试，可以通过引用一个路由调试库（RouteDebug.dll)进行调试
             //注册一条路由规则
+
+            routes.MapRoute(
+                name: "Test",
+                url: "{controller}/{action}/{id}",
+                defaults: new
+                {
+                    controller = "Test",   //默认值，可以有多条
+                    action = "Index",
+                    id = UrlParameter.Optional}
+                );
             routes.MapRoute(
                 name: "Default",  //作为路由规则的key，不能重复
                 url: "{controller}/{action}/{id}", //请求后台url规则
---
+++ Fashion/Fashion/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fashion.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            foreach(string file in Request.Files)
+            {
+                HttpPostedFileBase postFile = Request.Files[file];
+                ViewData["aa"] = postFile.FileName;
+                
+            }
+            
+            //string filename = Request["aa"].ToString();
+            //ViewData["aa"] = filename;
+            return View();
+        }
+
+    }
+}
