1a0fef8eb4fb6650bc23789bc8a9afbe29311fff
chenxudong <248200851@qq.com>
2016-03-27 11:13:15 +0800

    新建一个MyScripts用来保存自己的编写js文件，并新建一个ImageFun.js，把getB86b63b0 当用户登录成功后，显示自己的用户——_Layout.cshtml

---
+++ Fashion/Fashion/Scripts/MyScripts/ImageFun.js
@@ -0,0 +1,15 @@
+﻿//自己实现的一些javascript函数
+
+//此函数用户获取图片的base64数据
+//用于页面：Change_Data.cshtml
+//参数，img：图片元素
+//返回值：图片的去掉文件头的base64数据
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL.substr(dataURL.indexOf(",") + 1)//去掉base64的格式头，因为格式头不属于图片的数据，否则后台将图片的base64保存为图片时会出错
+}
