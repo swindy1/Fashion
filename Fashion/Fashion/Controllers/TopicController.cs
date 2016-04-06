21f2872645fb24730b5fab0f690f1c617a0b74f6
chenxudong <248200851@qq.com>
2016-04-06 20:50:14 +0800

    优化代码

--- Fashion/Fashion/Controllers/TopicController.cs
+++ Fashion/Fashion/Controllers/TopicController.cs
@@ -113,34 +113,27 @@ namespace Fashion.Controllers
         public ActionResult postData()
         {
             ////先把前端传回来的content内容保存为静态页面
-            //定义一个字节数组保存前端传回来的Post数据
-            byte[]byteData=new byte[Request.InputStream.Length];
-            //将流读取到byteData，InputStream读取到的是http头里的主体数据
-            Request.InputStream.Read(byteData,0,byteData.Length);
+            byte[] byteData = new byte[Request.InputStream.Length]; //定义一个字节数组保存前端传回来的Post数据
+            Request.InputStream.Read(byteData, 0, byteData.Length);//将流读取到byteData，InputStream读取到的是http头里的主体数据
             string postData = System.Text.Encoding.Default.GetString(byteData);
             postData = Server.UrlDecode(postData);
-            //从postData提取出前端传回来的评论内容，即变量名为content的数据
-            string[] datas = postData.Split('&');
-            string contentData = datas[1].ToString();
-            //去除变量名，如content=aaa，只取出aaa
-            contentData = contentData.Substring(contentData.IndexOf('=')+1);
-            
-            //定义文件名fileName
-            DateTime datetime = DateTime.Now;  
-            string fileName = datetime.ToString("yyyyMMddHHmmss_ffff") + ".html";
-            string fileNamePath = Server.MapPath("~/StaticHtml/TieZiHtml/") + fileName;
-            //先判断文件是否存在，若存在：更换文件名,最后该文件名html文件
-            while(System.IO.File.Exists(fileNamePath))
+            string[] datas = postData.Split('&');//对postData数据进行分割
+            string contentData = datas[1].ToString(); //data[1]为变量名为content的内容
+            contentData = contentData.Substring(contentData.IndexOf('=') + 1);//去除变量名，如content=aaa，只取出aaa
+            DateTime datetime = DateTime.Now;
+            string fileName = datetime.ToString("yyyyMMddHHmmss_ffff") + ".html";//定义文件名fileName
+            string fileNamePath = Server.MapPath("~/StaticHtml/TieZiHtml/") + fileName;//物理路径
+            while (System.IO.File.Exists(fileNamePath))//先判断文件是否存在，若存在：更换文件名
             {
-                fileNamePath=Server.MapPath("~/StaticHtml/TieZiHtml/") + fileName;
+                datetime = DateTime.Now;
+                fileName = datetime.ToString("yyyyMMddHHmmss_ffff") + ".html";
+                fileNamePath = Server.MapPath("~/StaticHtml/TieZiHtml/") + fileName;
             }
             System.IO.FileStream fs = new System.IO.FileStream(fileNamePath,System.IO.FileMode.Create);
             byte[] contentBytes = System.Text.Encoding.Default.GetBytes(contentData);
             fs.Write(contentBytes,0,contentBytes.Length);
-            fs.Close();
-            ///////保存静态html成功
-
-            //将帖子数据保存到数据库
+            fs.Close();//保存静态html成功
+            ///////将帖子数据保存到数据库
             Post_bll Post = new Post_bll();
             Theme_bll themeName = new Theme_bll();
             People_bll User = new People_bll();
@@ -149,47 +142,38 @@ namespace Fashion.Controllers
             int userId = User.GainUserId(userName);
             string theme = Request["theme"].ToString();
             int themeId = themeName.CollocateThemeId(theme);
-            string staticHtmlPath = "~/StaticHtml/TieZiHtml/" + fileName;
-            string content200 = datas[3].ToString();
+            string staticHtmlPath = "~/StaticHtml/TieZiHtml/" + fileName;//相对路径
+            string content200 = datas[3].ToString();//data[3]的为前端传回来的发帖内容的纯文本
             content200 = content200.Substring(content200.IndexOf('=') + 1);
-       
-            //过滤掉content200里图片
             System.Text.RegularExpressions.Regex regexImg = new System.Text.RegularExpressions.Regex(@"<img[^>]+>");
-            content200 = regexImg.Replace(content200, "");
-            //如果contentData的长度超过200,取contentData里的前两百个字符，将用于保存到数据库
+            content200 = regexImg.Replace(content200, "");//过滤掉content200里图片
             int len = content200.Length;
-            if (len > 200)
+            if (len > 200)//如果content200的长度超过200,取content200里的前两百个字符，将用于保存到数据库
             {
                 len = 200;
             }
             content200 = content200.Substring(0, len);  
-            //过滤掉所有的html标签
-            //content200 = System.Text.RegularExpressions.Regex.Replace(content200, @"<[^>]*>", String.Empty);
             if (Post.finshInsert(caption, content200, userId, themeId, staticHtmlPath,datetime) != 1)
             {
                 return Content("保存帖子信息时数据库出错");
-            }
+            }//将帖子数据保存到数据库---------成功
             //////获取所有图片里的图片路径,并且将图片路径保存到数据库里
-            // 定义正则表达式用来匹配 img 标签
-            System.Text.RegularExpressions.Regex regImg2 = new System.Text.RegularExpressions.Regex(@"<img\b[^<>]*?\bsrc[\s\t\r\n]*=[\s\t\r\n]*[""']?[\s\t\r\n]*(?<imgUrl>[^\s\t\r\n""'<>]*)[^<>]*?/?[\s\t\r\n]*>", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            System.Text.RegularExpressions.Regex regImg2 = new System.Text.RegularExpressions.Regex(@"<img\b[^<>]*?\bsrc[\s\t\r\n]*=[\s\t\r\n]*[""']?[\s\t\r\n]*(?<imgUrl>[^\s\t\r\n""'<>]*)[^<>]*?/?[\s\t\r\n]*>", System.Text.RegularExpressions.RegexOptions.IgnoreCase);// 定义正则表达式用来匹配 img 标签
             System.Text.RegularExpressions.MatchCollection matches = regImg2.Matches(contentData);
             int i = 0;
             string[] strUrlList = new string[matches.Count];
-            // 取得匹配项列表
             foreach (System.Text.RegularExpressions.Match match in matches)
             {
-                strUrlList[i++] = match.Groups["imgUrl"].Value;
+                strUrlList[i++] = match.Groups["imgUrl"].Value;// 取得匹配项列表
             }
             if(strUrlList.Length>=1)
-            { 
-            //根据帖子的标题查询数据库，得到该贴子的postId
-            int postId = Post.GetPostId(caption);
-            PostPhoto_bll postPhoto_bll = new PostPhoto_bll();
-                
-            if (postPhoto_bll.InsertPhotoUrl(postId, strUrlList) < 0)
             {
-                return Content("保存图片路径时数据库出错");
-            }
+                int postId = Post.GetPostId(caption); //根据帖子的标题查询数据库，得到该贴子的postId
+                PostPhoto_bll postPhoto_bll = new PostPhoto_bll();
+                if (postPhoto_bll.InsertPhotoUrl(postId, strUrlList) < 0)
+                {
+                    return Content("保存图片路径时数据库出错");
+                }
             }
             return Content("成功");
         }
