052d162669d01ff7244facb604274d23a757b2b2
chenxudong <248200851@qq.com>
2016-04-14 00:15:16 +0800

    实现Home页面里的评论功能

--- Fashion/Fashion/Code/DAL/PostComment_dal.cs
+++ Fashion/Fashion/Code/DAL/PostComment_dal.cs
@@ -39,6 +39,45 @@ namespace Fashion.Code.DAL
             return postComment_modelList;
         }
 
+        /// <summary>
+        /// 向数据库插入一条帖子评论
+        /// 成功返回1，错误返回0
+        /// </summary>
+        /// <param name="postId">帖子编号</param>
+        /// <param name="commenterId">评论者</param>
+        /// <param name="beCommenterId">被评论者</param>
+        /// <param name="content">评论内容</param>
+        /// <param name="datetime">评论时间</param>
+        /// <param name="supportCount">点赞数</param>
+        /// <param name="postType">帖子类型</param>
+        /// <returns></returns>
+        public int InsertComment(int postId,int commenterId,int beCommenterId,string content,DateTime datetime,int postType)
+        {
+            string sqlStr=@"insert tb_PostComment(Comment_PostId,Comment_CommenterId,
+                                                                               Comment_BeCommenterId,Comment_Content,
+                                                                               Conment_Date,Conment_SupportCount,
+                                                                               Conment_PostType)
+                                                                     values(@postId,@commenterId,@beCommenterId,
+                                                                                @content,@datetime,0,@postType)";
+            SqlParameter[]parameters=new SqlParameter[]{
+                new SqlParameter("@postId",postId),
+                new SqlParameter("@commenterId",commenterId),
+                new SqlParameter("@beCommenterId",beCommenterId),
+                new SqlParameter("@content",content),
+                new SqlParameter("@datetime",datetime),                
+                new SqlParameter("@postType",postType),
+            };
+            if (SqlHelper.ExecuteNonquery(sqlStr, parameters) == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
         public PostComment_model ToModel(DataRow row)
         {
       
