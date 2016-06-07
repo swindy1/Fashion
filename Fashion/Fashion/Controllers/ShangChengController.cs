5c975ef2cd94017a8bc688712e6a68ad03dfe258
chenxudong <248200851@qq.com>
2016-06-07 14:59:36 +0800

    添加商城和Header模板布局

---
+++ Fashion/Fashion/Controllers/ShangChengController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fashion.Controllers
+{
+    public class ShangChengController : Controller
+    {
+        //
+        // GET: /ShangCheng/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
